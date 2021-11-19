56f1d2c8d3fddd6d0c0d144048c614aa738b5b47
Fredy <fredy.huachaca21@gmail.com>
2021-11-18 19:10:34 -0500

    First commit

---
+++ src/main/java/com/pe/killa/webflux/opeadores/operadores_reactivos/OperadoresReactivosApplication.java
@@ -0,0 +1,13 @@
+package com.pe.killa.webflux.opeadores.operadores_reactivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperadoresReactivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperadoresReactivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/killa/webflux/opeadores/operadores_reactivos/OperadoresReactivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.killa.webflux.opeadores.operadores_reactivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadoresReactivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
