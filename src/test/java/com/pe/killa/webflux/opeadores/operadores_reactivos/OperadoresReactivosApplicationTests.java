958a5baf7459c6f505298176f992865991cf2d7d
Fredy <fredy.huachaca21@gmail.com>
2022-11-12 11:33:18 -0500

    Add dependency Test and First Test Mono

--- src/test/java/com/pe/killa/webflux/opeadores/operadores_reactivos/OperadoresReactivosApplicationTests.java
+++ src/test/java/com/pe/killa/webflux/opeadores/operadores_reactivos/OperadoresReactivosApplicationTests.java
@@ -2,12 +2,28 @@ package com.pe.killa.webflux.opeadores.operadores_reactivos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class OperadoresReactivosApplicationTests {
 
+	private static List<String> nombres = Arrays.asList("Fredy", "Gianella", "Frank", "Alex", "Marck", "Aracely", "Blanca", "juan");
+
+
 	@Test
-	void contextLoads() {
+	void testMonoDataInt() {
+			Mono<Integer> monoNum =	Mono.just(10);
+			monoNum.subscribe(x -> System.out.println("mono: " + x));
+
+		StepVerifier.create(monoNum)
+				.expectNext(10)
+				.expectComplete()
+				.verify();
+
 	}
 
 }
